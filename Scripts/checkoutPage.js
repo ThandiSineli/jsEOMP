1df266ea4b36f1cbb31aa65ad32d670c78c2a1da
Thandeka Sineli <thandekasineli@gmail.com>
2023-12-08 06:32:07 +0200

    checkout

--- Scripts/checkoutPage.js
+++ Scripts/checkoutPage.js
@@ -1,29 +1,43 @@
-let bought = JSON.parse(localStorage.getItem('purchaseditems'))
-let table= document.querySelector('table')
-table.innerHTML = bought.map((item, index) => {
-    
-    return `
-    <table>
-    <td>${index+1}</td>
-    <td>${item.name}</td>
-    <td>${item.description}</td>
-    <td>R${item.price}</td>
-    <td><img src='${item.url}'></img></td>
-    </table>
-    `
-})
 
-// try to calculate the toatal price of the items using reduce.
-//  let totalBought= bought.reduce((acc,item) => (acc + item.price * item.price) , 0 ) ;
-//  console.log("Total:"=totalBought)
+let purchaseditems = JSON.parse(localStorage.getItem('purchaseditems')) || [];
+let table = document.querySelector('#checkoutTable');
+let totalAmountElement = document.querySelector('#totalAmount');
 
+function displayCheckout() {
+    let tableContent = '';
 
+    if (purchaseditems.length === 0) {
+        tableContent = '<tr><td colspan="5">No items in the cart</td></tr>';
+    } else {
+        tableContent = `
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Image</th>
+            </tr>
+        `;
 
+        purchaseditems.forEach(item => {
+            tableContent += `
+                <tr>
+                    <td>${item.name}</td>
+                    <td>${item.description}</td>
+                    <td>R${item.price}</td>
+                    <td>${item.quantity}</td>
+                    <td><img src='${item.url}' alt='Product Image'></td>
+                </tr>
+            `;
+        });
+    }
 
- 
+    table.innerHTML = tableContent;
 
+    let totalAmount = purchaseditems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    totalAmountElement.textContent = `Total Amount: R${totalAmount.toFixed(2)}`;
+}
 
-
-
-
- 
+window.onload = function() {
+    displayCheckout();
+};
